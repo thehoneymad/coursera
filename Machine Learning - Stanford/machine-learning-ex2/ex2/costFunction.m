5a7767b380ed685051827756eab51fa4cc5a60eb
Swagata Prateek <thehoneymad@hotmail.com>
2017-07-16 23:03:44 +0200

    Excercise 2: Predict method added

--- Machine Learning - Stanford/machine-learning-ex2/ex2/costFunction.m
+++ Machine Learning - Stanford/machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,10 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta);
+inner_sum = (-y' * log(h) - (1 - y)' * log(1-h));
+J = (1/m) * inner_sum;
 
-
-
-
+grad = 1/m * X' * (h - y);
 
 
 
