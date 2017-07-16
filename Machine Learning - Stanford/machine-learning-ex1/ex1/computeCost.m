bf2b46d96e4820e8c0dc72384fded05f55711ef4
Swagata Prateek <thehoneymad@hotmail.com>
2017-07-16 22:06:30 +0200

    Initial commit

---
+++ Machine Learning - Stanford/machine-learning-ex1/ex1/computeCost.m
@@ -0,0 +1,26 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+inner_sum = ((theta' * X')' - y).^2;
+total_error = sum(inner_sum, 1);
+
+J = total_error / (2*m);
+
+% rounding off just to make sure that the grader don't misunderstand the result
+% J = round(J * 100) / 100;
+
+% =========================================================================
+
+end
