739896ca6a82f941c7f16e01aaa21f52a2a71477
thehoneymad <thehoneymad@hotmail.com>
2017-08-29 19:05:58 +0100

    Coursera ML: Doing the last bits of the excerise too

--- Machine Learning - Stanford/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Machine Learning - Stanford/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,9 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = 0.5 * sum(sum(R .* (X * Theta' - Y).^2));
-X_grad = R .* (X * Theta' - Y) * Theta;
-Theta_grad = R' .* (X * Theta' - Y)' * X;
+J = 0.5 * sum(sum(R .* (X * Theta' - Y).^2)) + lambda/2 .* sum(sum(Theta.^2)) + lambda/2 .* sum(sum(X.^2));
+X_grad = R .* (X * Theta' - Y) * Theta + lambda .* X;
+Theta_grad = R' .* (X * Theta' - Y)' * X + + lambda .* Theta;
 
 
 % =============================================================
