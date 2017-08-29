502023a14c5881069230da7679a9740d8e7bae7e
thehoneymad <thehoneymad@hotmail.com>
2017-08-29 18:56:53 +0100

    Coursera ML: Collaborative filtering gradients added

--- Machine Learning - Stanford/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Machine Learning - Stanford/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,10 +41,8 @@ Theta_grad = zeros(size(Theta));
 %
 
 J = 0.5 * sum(sum(R .* (X * Theta' - Y).^2));
-
-
-
-
+X_grad = R .* (X * Theta' - Y) * Theta;
+Theta_grad = R' .* (X * Theta' - Y)' * X;
 
 
 % =============================================================
